84a8aee1eddd09b83817841c18a4bd961c41dfbe
willyannedfl-coder <willyannedfl@gmail.com>
2026-08-13 23:34:18 +0000

    vercell

--- react-projeto/app/page.js
+++ react-projeto/app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="https://programa-o-front-end-xivg.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -47,7 +47,7 @@ export default function Home() {
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://programa-o-front-end-xivg.vercel.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
