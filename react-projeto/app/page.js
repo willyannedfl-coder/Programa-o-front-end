cb6d3a2ca1b5449235230674c0ec3d49f2995fb6
willyannedfl-coder <willyannedfl@gmail.com>
2026-08-14 00:22:50 +0000

    sim

--- react-projeto/app/page.js
+++ react-projeto/app/page.js
@@ -2,8 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 
-export default function Home() {
-  return (
+
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>hello mundo </h1>
