550dccb87376d9d814e224e97f02db4ecea33b28
willyannedfl-coder <willyannedfl@gmail.com>
2026-08-14 00:13:56 +0000

    sim

--- react-projeto/app/page.js
+++ react-projeto/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 import Gallery from './Gallery.js';
+import { Profile } from './Gallery.js';
 
 export default function App() {
   return (
